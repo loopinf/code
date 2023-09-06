f73d4e0426da7460e7a7db27a21c3c98fa058b4d
loopinf <loopinf@gmail.com>
2023-09-06 10:27:28 +0900

    init 0906

--- signal/1.mysignalhandler.c
+++ signal/1.mysignalhandler.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+//1.mysignalhandler.c
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
@@ -6,24 +6,14 @@ void sigint_handler(int signo)
 {
   printf("recv SIGINT!\n");
   psignal(signo, "Received signal");
-  sleep(5);
-  printf("done %d\n", signo);
 }
 int main(void)
 {
   printf("my pid %d\n", getpid());
-  if (signal(SIGINT, sigint_handler) ==  SIG_ERR){
+  if (signal(SIGINT, sigint_handler) == SIG_ERR) {
     perror("Cannot handle SIGINT!\n");
     _exit(0);
   }
-  if (signal(SIGUSR1, sigint_handler) ==  SIG_ERR){
-    perror("Cannot handle SIGINT!\n");
-    _exit(0);
-  }
-  if (signal(SIGTSTP, sigint_handler) ==  SIG_ERR){
-    perror("Cannot handle SIGINT!\n");
-    _exit(0);
-  }
-  for (;;) pause();
+  for (;;) pause(); // pause(2) : waiting a signal (any signal)
   return 0;
-}
+} 
