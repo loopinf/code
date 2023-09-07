724dd1b7c53f0903d6c1d32e482efcbcce996775
loopinf <loopinf@gmail.com>
2023-09-07 13:36:37 +0900

    .

--- day3/5.thread_example/1.thread.c
+++ day3/5.thread_example/1.thread.c
@@ -1,19 +1,27 @@
-#include <pthread.h> 
-#include <stdio.h> 
-#include <unistd.h> 
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 
-void* thread_routine(void *arg){  
-  printf("\n\nth func - tid:%lx\n", pthread_self());   
-  for(;;){}
-} 
+void *thread_routine(void *arg)
+{
+  printf("\n\nth func - tid:%lx\n", pthread_self());
+  for (;;)
+  {
+  }
+}
 
-int main(){    
-//Q-- 쓰레드를 2개를 생성해주세요.
+int main()
+{
+  // Q-- 쓰레드를 2개를 생성해주세요.
+  pthread_t tid1;
+  pthread_t tid2;
+  pthread_create(&tid1, NULL, thread_routine, NULL);
+  pthread_create(&tid2, NULL, thread_routine, NULL);
 
+  pthread_join(tid1, NULL);
 
-
-
-
-  printf("\n\ntid:%lx\n",pthread_self());  
-  for(;;){}
+  printf("\n\ntid:%lx\n", pthread_self());
+  for (;;)
+  {
+  }
 }
