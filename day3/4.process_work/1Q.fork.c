e2f0be3dc7a5b2d90ff5e17c2dd664ba8ea686d3
loopinf <loopinf@gmail.com>
2023-09-07 09:52:39 +0900

    my answer

--- day3/4.process_work/1Q.fork.c
+++ day3/4.process_work/1Q.fork.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
  #include <sys/wait.h>
 static long long num = 0;
+static int pid = 0;
 int main(void)
 {  
   printf("start\n");
@@ -18,10 +19,30 @@ int main(void)
 //Q--
 //Q--두 프로세스로 아래 연산을 수행하도록 코드 수정해주세요
 //Q--절반씩 동시 작업이 되도록 코드를 변경해주세요
-  for (long long i = 1; i < 5000000000; i++)
-  {
-    num += 1;
+
+  pid = fork(); // pid == 0 child process
+
+  if (pid==0) {
+    for (long long i = 1; i < 25000000; i++)
+    {
+      num += 1;
+    }
   }
+  else if (pid>0){
+    for (long long i = 25000001 ; i <  5000000000; i++)
+    {
+      num += 1;
+    }
+  }
+  else {
+    fprintf(stderr, "Fork failed");
+    exit(-1);
+  }
+  
+  // for (long long i = 1; i < 5000000000; i++)
+  // {
+  //   num += 1;
+  // }
 
   t2 = times(&buf);
 
